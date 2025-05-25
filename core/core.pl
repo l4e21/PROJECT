b3b8e4d2fc72b88af72774c889cf2b1965ccabb4
Jam <JWalkerdine21@hotmail.com>
2025-05-25 12:11:37 +0100

    First commit

---
+++ core/core.pl
@@ -0,0 +1,26 @@
+:- module(core, [call_slot/1, slot/3]).
+
+:- dynamic(slot/3).
+
+%% call_slot is a dispatch shell, in the future we can flesh it out and provide more hooks, as well as allowing new metaobjects to define their own call_slot capabilities.
+call_slot(SlotHead) :-
+    SlotHead =.. [_, ID|_],
+    call_slot(ID, SlotHead).
+    
+call_slot(CallerID, SlotHead) :-
+    (slot(CallerID, SlotHead, Body)
+    -> Body
+    ; (slot(CallerID, inherits(CallerID, InheritsID), Body),
+       Body,
+       call_slot(InheritsID, SlotHead))).
+
+%% The only starting object is root, a metaobject which determines how new objects are created.
+slot(root, make_obj(Self, Slots, ID),
+     (gensym(obj_, ID),
+      call_slot(make_slots(Self, ID, Slots)))).
+
+slot(root, make_slots(_Self, _, []), true).
+slot(root, make_slots(Self, ID, [(SlotHead, SlotBody)|Slots]),
+     (asserta(slot(ID, SlotHead, SlotBody)),
+      call_slot(make_slots(Self, ID, Slots)))).
+
---
+++ examples/flashcards.pl
@@ -0,0 +1,38 @@
+
+:- module(flashcards_example, []).
+
+:- use_module("../core/core.pl", [call_slot/1, slot/3]).
+
+flashcard_init(ID, Slots) :-
+    call_slot(make_obj(root, [(card_name(_Self, ren), true)], ID)),
+    findall(Head-Body, slot(ID, Head, Body), Slots),
+    length(Slots, 1).
+
+%% We can look up an object by seeing if it implements a given predicate.
+flashcard_ids(Cards) :-
+    findall(ID, (slot(ID, card_name(_, _), _)), Cards).
+
+flashcard_factory_init(FactoryID, Slots) :-
+    call_slot(make_obj(root,
+                       [(make_card(_FactorySelf, CardID, CardName),
+                         call_slot(make_obj(root,
+                                            [(card_name(_CardSelf, CardName), true)],
+                                            CardID)))],
+                       FactoryID)),
+    findall(Head-Body, slot(FactoryID, Head, Body), Slots).
+
+make_card(FactoryID, CardID, CardSlots) :-
+    call_slot(make_card(FactoryID, CardID, ren2)),
+    findall(Head-Body, slot(CardID, Head, Body), CardSlots).
+
+% ?- flashcard_init(CardID, CardSlots), flashcard_ids(Cards).
+
+% ?- flashcard_factory_init(FactoryID, FactorySlots), make_card(FactoryID, CardID, CardSlots).
+%@ Correct to: "flashcards_example:flashcard_factory_init(FactoryID,FactorySlots)"? yes
+%@ Correct to: "flashcards_example:make_card(FactoryID,CardID,CardSlots)"? yes
+%@ FactoryID = obj_1,
+%@ FactorySlots = [make_card(_, _A, _B)-call_slot(make_obj(root, [(card_name(_, _B), true)], _A))],
+%@ CardID = obj_2,
+%@ CardSlots = [card_name(_, ren2)-true].
+
+% ?- slot(obj_3, Head, Body).
