9a1c210d48d029a693f8b4e83ceb958ca2cee2ab
Jam <JWalkerdine21@hotmail.com>
2025-05-26 13:03:14 +0100

    Discovered need for the bitemporal metaobject

--- examples/chinese.pl
+++ examples/chinese.pl
@@ -3,122 +3,46 @@
 :- use_module("../core/core.pl", [call_slot/1, slot/3]).
 :- use_module(library(clpfd)).
 
-month_name(1, "January").
-month_name(2, "February").
-month_name(3, "March").
-month_name(4, "April").
-month_name(5, "May").
-month_name(6, "June").
-month_name(7, "July").
-month_name(8, "August").
-month_name(9, "September").
-month_name(10, "October").
-month_name(11, "November").
-month_name(12, "December").
+default_date(Stamp) :-
+    date_time_stamp(date(2000, 1, 1, 0, 0, 0.0, 0, 'local', -), Stamp).
 
-days_in_month(1, 31).
-days_in_month(2, 28).
-days_in_month(3, 31).
-days_in_month(4, 30).
-days_in_month(5, 31).
-days_in_month(6, 30).
-days_in_month(7, 31).
-days_in_month(8, 31).
-days_in_month(9, 30).
-days_in_month(10, 31).
-days_in_month(11, 30).
-days_in_month(12, 31).
+stamp_to_date9(Stamp, DateTime) :- stamp_date_time(Stamp, DateTime, local).
 
-valid_time(Hr:Min) :-
-    Hr in 0..23,
-    Min in 0..59.
-
-valid_date(_Yr/Month/Day) :-
-    Month in 1..12,
-    days_in_month(Month, DaysInMonth),
-    between(1, DaysInMonth, Day).
-
-make_time(Yr/Month/Day, Hr:Min, ID) :-
-    valid_date(Yr/Month/Day),
-    valid_time(Hr:Min),
-    call_slot(make_obj(root,
-                       [
-                           (year(Self, Yr), true),
-                           (month(Self, Month), true),
-                           (day(Self, Day), true),
-                           
-                           (hour(Self, Hr), true),
-                           (min(Self, Min), true),
-                           (time_term(Self, Yr/Month/Day, Hr:Min),
-                            (call_slot(year(Self, Yr)),
-                             call_slot(month(Self, Month)),
-                             call_slot(day(Self, Day)),
-                             call_slot(hour(Self, Hr)),
-                             call_slot(min(Self, Min))))
-                       ],
-                       ID)).
-
-now(ID) :-
-    get_time(Stamp),
-    stamp_date_time(Stamp, DateTime, local),
-    date_time_value(year, DateTime, Yr),
-    date_time_value(month, DateTime, Month),
-    date_time_value(day, DateTime, Day),
-    date_time_value(hour, DateTime, Hr),
-    date_time_value(minute, DateTime, Min),
-    make_time(Yr/Month/Day, Hr:Min, ID).
-
-before(T1, T2) :-
-    call_slot(time_term(T1, Date1, Time1)),
-    call_slot(time_term(T2, Date2, Time2)),
-    before(Date1, Time1, Date2, Time2).
-
-before(Yr1/_/_, _:_, Yr2/_/_, _:_) :-
-    Yr1 #< Yr2.
-before(Yr1/Month1/_, _:_, Yr2/Month2/_, _:_) :-
-    Yr1 #= Yr2,
-    Month1 #< Month2.
-before(Yr1/Month1/Day1, _:_, Yr2/Month2/Day2, _:_) :-
-    Yr1 #= Yr2,
-    Month1 #= Month2,
-    Day1 #< Day2.
-before(Yr1/Month1/Day1, Hr1:_, Yr2/Month2/Day2, Hr2:_) :-
-    Yr1 #= Yr2,
-    Month1 #= Month2,
-    Day1 #= Day2,
-    Hr1 #< Hr2.
-before(Yr1/Month1/Day1, Hr1:Min1, Yr2/Month2/Day2, Hr2:Min2) :-
-    Yr1 #= Yr2,
-    Month1 #= Month2,
-    Day1 #= Day2,
-    Hr1 #= Hr2,
-    Min1 #< Min2.
-
-% ?- chinese_example:now(ID), call_slot(year(ID, Yr)).
+time_delta(Stamp1, Stamp2, Days) :-
+    Delta is Stamp1 - Stamp2,
+    Days is floor(Delta / 86400).
 
 make_flashcard(English, Pinyin, Character, CardID) :-
-    make_time(2000/1/1, 0:0, DefaultTimeID),
+    default_date(DefaultTimeStamp),
     call_slot(make_obj(root,
                        [
                            (english(Self, English), true),
                            (pinyin(Self, Pinyin), true),
                            (character(Self, Character), true),
                            
-                           (last_answered(Self, DefaultTimeID), true),
-                           (last_shown(Self, DefaultTimeID), true),
-                           (correct_answers(Self, 0), true),
+                           (last_shown(Self, DefaultTimeStamp), true),
+                           (confidence(Self, 1), true),
                            (card(Self), true),
 
-                           (answer(Self),
-                            (chinese_example:now(NowID1),
-                             call_slot(correct_answers(Self, N)),
-                             N1 is N + 1,
-                             call_slot(make_slots(root, Self, [(last_answered(SelfAux, NowID1), true),
-                                                               (correct_answers(SelfAux, N1), true)])))),
-                           (show(Self, ToShow),
-                            (call_slot(character(Self, ToShow)),
-                             chinese_example:now(NowID2),
-                             call_slot(make_slots(root, Self, [(last_shown(_SelfAux, NowID2), true)]))))
+                           (needs_practice(Self),
+                            get_time(NowStamp),
+                            call_slot(confidence(Self, Confidence)),
+                            call_slot(last_shown(Self, LastShownStamp)),
+                            print("HI"),
+                            chinese_example:time_delta(NowStamp, LastShownStamp, DaysSinceShown),
+                            print(DaysSinceShown),
+                            Limit is 2**Confidence,
+                            print(Limit),
+                            DaysSinceShown > Limit),
+
+                           (answer(Self, RW),
+                            (call_slot(confidence(Self, N)),
+                             (RW == right, N1 is N + 1
+                             ; RW == wrong, N1 is 1),
+                             call_slot(make_slots(root, Self, [(confidence(SelfAux, N1), true)])))),
+                           (show(Self),
+                            (get_time(NowStamp),
+                             call_slot(make_slots(root, Self, [(last_shown(SelfAux, NowStamp), true)]))))
                        ],
                        CardID)).
 
@@ -127,34 +51,27 @@ flashcards(FlashcardIDs) :-
             slot(FlashcardID, card(_Self), true),
             FlashcardIDs).
 
-%% oldest_flashcard(FlashcardID) :-
-%%     flashcards(FlashcardIDs),
-%%     oldest_flashcard(FlashcardID, FlashcardIDs).
-
-%% oldest_flashcard(FlashcardID, [FlashcardID]).
-%% oldest_flashcard(Oldest, [FlashcardID|Flashcards]) :-
-%%     oldest_flashcard(FlashcardID2, Flashcards),
-%%     call_slot(last_shown(FlashcardID, LastShownID)),
-%%     call_slot(last_shown(FlashcardID2, LastShownID2)),
-%%     (before(LastShownID, LastShownID2)
-%%     -> Oldest = FlashcardID
-%%     ; Oldest = FlashcardID2).
 
 
-make_deck(DeckID) :-
+make_deck(DeckID, Name) :-
     call_slot(make_obj(root,
                        [
-                           (add_card(Self, CardID1), call_slot(make_slots(root, Self, [(card(_SelfAux, CardID1), true)]))),
-                           (all_cards(Self, CardIDs2), findall(CardID2, call_slot(card(Self, CardID2)), CardIDs2)),
-                           (oldest_flashcard(Self, CardID3), (call_slot(all_cards(Self, CardIDs3)),
-                                                             call_slot(oldest_flashcard(Self, CardID3, CardIDs3)))),
-                           (oldest_flashcard(Self, CardID5, [F1|CardIDs5]), (call_slot(oldest_flashcard(Self, F2, CardIDs5)),
-                                                                             call_slot(last_shown(F1, LS1)),
-                                                                             call_slot(last_shown(F2, LS2)),
-                                                                             (chinese_example:before(LS1, LS2), CardID5 = F1
-                                                                             ; CardID5 = F2))),
-                           (oldest_flashcard(Self, CardID4, [CardID4]), true),
-                           (show_next(Self, CardID5, Char), (call_slot(oldest_flashcard(Self, CardID5)), call_slot(show(CardID5, Char))))
+                           (name(Self, Name), true),
+                           
+                           (add_card(Self, CardID), call_slot(make_slots(root, Self, [(card(_SelfAux, CardID), true)]))),
+                           (all_cards(Self, CardIDs), findall(CardID, call_slot(card(Self, CardID)), CardIDs))
+
+                           %% Not a great algo, instead there should be some confidence-and-time ranking like Anki
+                           %% (oldest_flashcard(Self, CardID), (call_slot(all_cards(Self, CardIDs)),
+                           %%                                   call_slot(oldest_flashcard(Self, CardID, CardIDs)))),
+                           %% (oldest_flashcard(Self, CardID, [F1|CardIDs]), (call_slot(oldest_flashcard(Self, F2, CardIDs)),
+                           %%                                                   call_slot(last_shown(F1, LS1)),
+                           %%                                                   call_slot(last_shown(F2, LS2)),
+                           %%                                                   (chinese_example:before(LS1, LS2), CardID = F1
+                           %%                                                   ; CardID = F2))),
+                           %% (oldest_flashcard(Self, CardID, [CardID]), true),
+                           
+                           %% (show_next(Self, CardID, Char), (call_slot(oldest_flashcard(Self, CardID)), call_slot(chinese(CardID, Char)), call_slot(show(CardID))))
                            
                        ],
                        DeckID)).
@@ -162,22 +79,24 @@ make_deck(DeckID) :-
 deck_example(DeckID) :-
     make_flashcard(card1, pinyin1, char1, CardID),
     make_flashcard(card2, pinyin2, char2, CardID2),
-    make_deck(DeckID),
+    make_deck(DeckID, example),
     call_slot(add_card(DeckID, CardID)),
     call_slot(add_card(DeckID, CardID2)).
 
-flashcard_answer_update_example(ID, Date, Time, Date2, Time2) :-
+flashcard_answer_update_example(ID, LastShownDate, LastShownDate2) :-
     make_flashcard(person, rén, 人, ID),
-    call_slot(last_answered(ID, LastAnsweredTimeID)), call_slot(time_term(LastAnsweredTimeID, Date, Time)),
-    call_slot(answer(obj_2)),
-    call_slot(last_answered(obj_2, A)), call_slot(time_term(A, Date2, Time2)).
+    call_slot(last_shown(ID, LastShown)),
+    call_slot(needs_practice(ID)),
+    call_slot(show(ID)),
+    call_slot(last_shown(ID, LastShown2)),
+    %% not(call_slot(needs_practice(ID))),
+    stamp_to_date9(LastShown, LastShownDate),
+    stamp_to_date9(LastShown2, LastShownDate2).
 
-% ?- deck_example(DeckID), call_slot(oldest_flashcard(DeckID, Oldest)).
+% ?- slot(obj_3, A, B).
+% ?- deck_example(DeckID).
+% ?- call_slot(last_shown(obj_3, D)).
 
-% ?- flashcard_answer_update_example(ID, Date, Time, Date2, Time2).
+% ?- flashcard_answer_update_example(ID, Date1, Date2).
 
 % ?- qsave_program("../../chinese_flashcards", [stand_alone(true)]).
-%@ % Disabled autoloading (loaded 48 files)
-%@ % Disabled autoloading (loaded 4 files)
-%@ % Disabled autoloading (loaded 0 files)
-%@ true.
