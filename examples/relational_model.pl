faa1536a4066299b0abf1ebaf39325dfa3780e69
Jam <JWalkerdine21@hotmail.com>
2025-05-31 12:26:36 +0100

    make relational model example properly MOP

--- examples/relational_model.pl
+++ examples/relational_model.pl
@@ -1,12 +1,13 @@
 
-:- module(relational_model, []).
+:- module(relational_model_example, []).
 
 :- use_module("../core/core.pl", [call_slot/1, slot/3]).
 
-%% Tables should really just be metaobjects
-%% Then rows would be objects created by the table metaobject.
+%% I heard you like relational models
+%% so I put a simplified relational model (this example)
+%% inside of an object-oriented relational model (PROJECT)
+%% inside of a purely relational model (PROLOG)
 
-attributes_to_methods([], []).
 attributes_to_methods([AttrDict|Attributes], AttrMethods) :-
     get_dict(name, AttrDict, AttrName),
     get_dict(type, AttrDict, AttrType),
@@ -16,53 +17,43 @@ attributes_to_methods([AttrDict|Attributes], AttrMethods) :-
     ; AttrMethods1 = [(AttrMethod, true)]),
     attributes_to_methods(Attributes, AttrMethods2),
     append(AttrMethods1, AttrMethods2, AttrMethods).
-    
+
 validate_type(Type, Val) :-
     call(Type, Val).
 
-table_init(ID, Attributes) :-
-    attributes_to_methods(Attributes, AttrMethods),
+table_factory_init(FactoryID) :-
     call_slot(make_obj(root, [
-                           (put(Self, Row),
-                            (call_slot(validate_row(Self, Row)),
-                             call_slot(make_slots(root, Self, [(row(_, Row), true)])))),
-                           (validate_row(Self, Row),
-                               (not((call_slot(attribute(Self, Name, Type)),
-                                     (get_dict(Name, Row, Val) -> not(relational_model:validate_type(Type, Val))
-                                     ; true)))))
-                           |AttrMethods],
-                       ID)).
+                           (make_obj(Self, Attributes, TableID),
+                            (gensym(table_obj_, TableID),
+                             asserta(slot(TableID, metaobject(_, Self), true)),
+                             ValidateRow = (validate_row(TableSelf, Row),
+                                                       not((call_slot(attribute(TableSelf, Name, Type)),
+                                                            (get_dict(Name, Row, Val) -> not(relational_model_example:validate_type(Type, Val))
+                                                            ; true)))),
+                             PutRow = (put(TableSelf, Row), (call_slot(validate_row(TableSelf, Row)),
+                                                             call_slot(make_slots(Self, TableSelf, [(row(_, Row), true)])))),
+                             relational_model_example:attributes_to_methods(Attributes, AttrMethods),
+                             call_slot(make_slots(Self, TableID, [PutRow, ValidateRow|AttrMethods])))),
+                           (inherits(Self, root), true)
+                       ],
+                      FactoryID)).
 
+% ?- table_factory_init(ID), call_slot(make_obj(ID, [_{name: id, type: integer, key: true}], TableID)), call_slot(put(TableID, _{id: 3})), slot(TableID, row(_, Row), _).
+%@ Correct to: "relational_model_example:table_factory_init(ID)"? yes
+%@ Correct to: "core:call_slot(make_obj(ID,[_{key:true,name:id,type:integer}],TableID))"? yes
+%@ Correct to: "core:call_slot(put(TableID,_{id:3}))"? yes
+%@ Correct to: "core:slot(TableID,row(_,Row),_)"? yes
+%@ ID = obj_1,
+%@ TableID = table_obj_1,
+%@ Row = _{id:3} ;
+%@ false.
 
 % ?- table_init(ID, [_{name: id, type: integer, key: true}]), call_slot(put(ID, _{id: 3})).
-%@ Correct to: "relational_model:table_init(ID,[_{key:true,name:id,type:integer}])"? yes
-%@ Correct to: "core:call_slot(put(ID,_{id:3}))"? yes
-%@ ID = obj_1 ;
-%@ false.
 
 % ?- call_slot(attribute(obj_1, Name, Type)).
 % ?- call_slot(key(obj_1, Name)).
 
 % ?- slot(obj_1, Y, Z).
-%@ Correct to: "core:slot(obj_1,Y,Z)"? yes
-%@ Y = row(_, _{id:3}),
-%@ Z = true ;
-%@ Y = key(_, id),
-%@ Z = true ;
-%@ Y = attribute(_, id, integer),
-%@ Z = true ;
-%@ Y = validate_row(_A, _B),
-%@ Z = not((call_slot(attribute(_A, _C, _D)), (get_dict(_C, _B, _E)->not(relational_model:validate_type(_D, _E));true))) ;
-%@ Y = put(_A, _B),
-%@ Z = (call_slot(validate_row(_A, _B)), call_slot(make_slots(root, _A, [(row(_, _B), true)]))) ;
-%@ Y = metaobject(_, root),
-%@ Z = true.
 
 % ?- call_slot(metaobject(A, root)).
-%@ Correct to: "core:call_slot(metaobject(A,root))"? yes
-%@ A = obj_1 ;
-%@ false.
-%@ Correct to: "core:call_slot(metaobject(_,root))"? yes
-%@ true ;
-%@ false.
 
